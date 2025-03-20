0bc352a964c824ac0a30029cbfe664033dbbd3be
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-03-20 03:22:23 -0300

    added js functions

---
+++ script.js
@@ -0,0 +1,52 @@
+'strict mode';
+
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play & pause video
+const toggleVideoStatus = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// Update play/pause icon
+const updatePlayIcon = () => {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+};
+
+// Update Progress && Timestamp
+const updateProgress = () => {
+  return true;
+};
+
+// Set Vudeo time to progress
+const setVideoProgress = () => {
+  return true;
+};
+
+// Stop video
+const stopVideo = () => {
+  return true;
+};
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
